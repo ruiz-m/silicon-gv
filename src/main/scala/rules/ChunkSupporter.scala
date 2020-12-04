b3322a510ddfb74162433f9e2dab05bed0017bc6
Jacob Gorenburg <jgorenburg@haverford.edu>
2020-12-04 17:06:29 -0500

    cleaning up

--- src/main/scala/rules/ChunkSupporter.scala
+++ src/main/scala/rules/ChunkSupporter.scala
@@ -83,9 +83,6 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
              (Q: (State, Heap, Term, Verifier, Boolean) => VerificationResult)
              : VerificationResult = {
 
-  //  heuristicsSupporter.tryOperation[Heap, Term](description)(s, h, v)((s1, h1, v1, QS) => {
-
-
       consume(s, h, resource, args, perms, ve, v)((s1, h1, optSnap, v1) =>
         optSnap match {
           case Some(snap) =>
@@ -97,12 +94,10 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
              * Returning a fresh snapshot in these cases should not lose any information.
              */
 
-            print("got here \n")
             val fresh = v1.decider.fresh(sorts.Snap)
             val s2 = s1.copy(functionRecorder = s1.functionRecorder.recordFreshSnapshot(fresh.applicable))
             Q(s2, h1, fresh, v1, false)
         })
-//    })(Q)
   }
 
   private def consume(s: State,
@@ -116,19 +111,7 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
                      : VerificationResult = {
 
     val id = ChunkIdentifier(resource, Verifier.program)
-/*    if (s.exhaleExt) {
-      val failure = Failure(ve).withLoad(args)
-      magicWandSupporter.transfer(s, perms, failure, v)(consumeGreedy(_, _, id, args, _, _))((s1, optCh, v1) =>
-        Q(s1, h, optCh.flatMap(ch => Some(ch.snap)), v1))
-    } else {
-*/
-//      executionFlowController.tryOrFail2[Heap, Option[Term]](s.copy(h = h), v)((s1, v1, QS) =>
-/*        if (s.isMethodVerification && Verifier.config.enableMoreCompleteExhale()) {
-          moreCompleteExhaleSupporter.consumeComplete(s1, s1.h, resource, args, perms, ve, v1)((s2, h2, snap2, v2) => {
-            QS(s2.copy(h = s.h), h2, snap2, v2)
-          })
-        } else {
-*/
+
     val s1 = stateConsolidator.consolidate(s.copy(h = h), v)
     consumeGreedy(s1, s1.h, id, args, perms, v) match {
       case (Complete(), s2, h2, optCh2) =>
@@ -141,14 +124,7 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
       case (Incomplete(p), s2, h2, None) =>
         Q(s2.copy(h = s.h), h2, None, v)
 
-/*
-      case _ =>
-        createFailure(ve, v, s1, true).withLoad(args)
-*/
     }
-//        }
-//      )(Q)
-//    }
   }
 
   private def consumeGreedy(s: State,
@@ -157,19 +133,6 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
                             args: Seq[Term],
                             perms: Term,
                             v: Verifier) = {
-    // no longer used
-    val consumeExact = terms.utils.consumeExactRead(perms, s.constrainableARPs)
-
-
-    // no longer used
-    def assumeProperties(chunk: NonQuantifiedChunk, heap: Heap) = {
-      val interpreter = new NonQuantifiedPropertyInterpreter(heap.values, v)
-      val resource = Resources.resourceDescriptions(chunk.resourceID)
-      v.decider.assume(interpreter.buildPathConditionsForChunk(chunk, resource.instanceProperties))
-    }
-
-//    println("h")
-//    h.values.foreach(chunk => print(chunk + "\n"))
 
 /* the foldl portion of heap-rem
  * builds a new heap of chunks that definitely do not
@@ -186,13 +149,10 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
           else {
             currHeap
           }
-      case _ =>
-        currHeap
+        case _ =>
+          currHeap
       }
     }
-
-//    println("new chunk")
-//    newH.values.foreach(chunk => print(chunk + "\n"))
 
     // tries to find the chunk in h
     findChunk[NonQuantifiedChunk](h.values, id, args, v) match {
@@ -200,50 +160,9 @@ object chunkSupporter extends ChunkSupportRules with Immutable {
       // I'm not sure if I need these checks but I included them to be safe - J
       case Some(ch) if v.decider.check(ch.perm === perms, Verifier.config.checkTimeout()) && v.decider.check(perms === FullPerm(), Verifier.config.checkTimeout()) =>
           (Complete(), s, newH, Some(ch))
-/*
-//          println("ch: " + ch.id)
-//        if (consumeExact) {
-          val toTake = PermMin(ch.perm, perms)
-          val newChunk = ch.withPerm(PermMinus(ch.perm, toTake))
-//          println("newchunk: " + newChunk.id)
-          val takenChunk = Some(ch.withPerm(toTake))
-          var newHeap = h - ch
-          // I think this next line should only be for fractional perms
-*          if (!v.decider.check(newChunk.perm === NoPerm(), Verifier.config.checkTimeout())) {
-            newHeap = newHeap + newChunk
-            assumeProperties(newChunk, newHeap)
-          }
-*
-          (ConsumptionResult(PermMinus(perms, toTake), v, 0), s, newHeap, takenChunk)
-*        } else {
-          // we should never be in here
-          if (v.decider.check(ch.perm !== NoPerm(), Verifier.config.checkTimeout())) {
-            v.decider.assume(PermLess(perms, ch.perm))
-            val newChunk = ch.withPerm(PermMinus(ch.perm, perms))
-            val takenChunk = ch.withPerm(perms)
-            val newHeap = h - ch + newChunk
-            assumeProperties(newChunk, newHeap)
-            (Complete(), s, newHeap, Some(takenChunk))
-          } else {
-            (Incomplete(perms), s, h, None)
-          }
-        }
-*
-*/
+
       case _ =>
         (Incomplete(perms), s, newH, None)
-
-
-
-/*
-      case None =>
-        if (consumeExact && s.retrying && v.decider.check(perms === NoPerm(), Verifier.config.checkTimeout())) {
-          (Complete(), s, h, None)
-        } else {
-          (Incomplete(perms), s, h, None)
-        }
-*/
-
     }
   }
 
