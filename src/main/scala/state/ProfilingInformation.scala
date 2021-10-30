285c52757385c2a8ce602c4a7bef742a9de77876
Hemant Sai Gouni <hemant@hemantgouni.com>
2021-10-30 23:27:28 +0000

    Add profiling singleton object

---
+++ src/main/scala/state/ProfilingInformation.scala
@@ -0,0 +1,30 @@
+package viper.silicon.state
+
+import java.util.concurrent.atomic.AtomicInteger
+
+// WARNING: this must be changed to be local to individual methods! ie, this is
+// currently (probably) not thread safe in that way
+
+object profilingInfo {
+
+  private val eliminatedConjuncts = new AtomicInteger(0);
+
+  private val totalConjuncts = new AtomicInteger(0);
+
+  def incrementEliminatedConjuncts = eliminatedConjuncts.incrementAndGet()
+
+  def incrementEliminatedConjuncts(value: Int): Int = {
+    eliminatedConjuncts.addAndGet(value)
+  }
+
+  def incrementTotalConjuncts = totalConjuncts.incrementAndGet()
+
+  def incrementTotalConjuncts(value: Int): Int = {
+    totalConjuncts.addAndGet(value);
+  }
+
+  def getEliminatedConjuncts = eliminatedConjuncts.get()
+
+  def getTotalConjuncts = totalConjuncts.get()
+
+}
