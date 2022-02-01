ca1898621cc3d1b5ef9a8362f23cf6045d3bedd3
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-02-01 23:52:28 +0000

    Update MapUtils to reflect widened constraints

--- src/main/scala/supporters/MapUtils.scala
+++ src/main/scala/supporters/MapUtils.scala
@@ -1,21 +1,19 @@
 
 package viper.silicon.supporters
 
-import viper.silver.ast.Node
-
 import scala.util.hashing.Hashing
 
-class NodeHash[T <: Node] extends AnyRef with Hashing[T] {
+class NodeHash[T <: Any] extends AnyRef with Hashing[T] {
 
   def hash(call: T): Int = {
     call.hashCode()
   }
 }
 
-class NodeEquiv[T <: Node] extends AnyRef with Equiv[T] {
+class NodeEquiv[T <: AnyRef] extends AnyRef with Equiv[T] {
 
   def equiv(call1: T, call2: T): Boolean = {
-    call1.uniqueIdentifier == call2.uniqueIdentifier
+    call1 eq call2
   }
 }
 
