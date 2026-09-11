9d7465539b2b6d8fd1db5729df0a4d6ae2c98ee9
ruiz-m <ruiz2357m@gmail.com>
2026-09-11 15:57:38 -0400

    Fixed gradual_divide_error

--- src/main/scala/decider/TermToSMTLib2Converter.scala
+++ src/main/scala/decider/TermToSMTLib2Converter.scala
@@ -201,6 +201,7 @@ class TermToSMTLib2Converter
 
     case FullPerm => "$Perm.Write"
     case NoPerm => "$Perm.No"
+    case EpsilonPerm => "$Perm.No"
     case FractionPermLiteral(r) => renderBinaryOp("/", renderAsReal(IntLiteral(r.numerator)), renderAsReal(IntLiteral(r.denominator)))
     case FractionPerm(n, d) => renderBinaryOp("/", renderAsReal(n), renderAsReal(d))
     case PermLess(t0, t1) => renderBinaryOp("<", render(t0), render(t1))
